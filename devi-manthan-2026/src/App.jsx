864444ebd97d53010aa3f134890aef64210c4beb
installationsoftware <installationsoftware>
2026-04-16 18:49:38 +0530

    Removed MR. from proffessor's names

--- devi-manthan-2026/src/App.jsx
+++ devi-manthan-2026/src/App.jsx
@@ -28,7 +28,6 @@ function AppLayout() {
           <Route path="/about" element={<AboutPage />} />
           <Route path="/events" element={<EventsPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/admin" element={<AdminPage/>} />
         </Routes>
       </AnimatePresence>
     </GlobalLayout>
